a6173a3d8133e65bf2a71b65437daff551d7df94
Vyacheslav Soldatov <vvsfree@yandex.ru>
2020-03-25 15:50:43 +0600

    Декоративные адаптивные элементы. Каталог. Элементы карточки путешественника (без микросетки)

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
+
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
@@ -19,6 +24,19 @@ gulp.task("css", function () {
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/*.svg")
+    .pipe(plumber())
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename("sprite.svg"))
+    // .pipe(imagemin([imagemin.svgo({
+    //   plugins: [
+    //     { removeViewBox: false }
+    //   ]
+    // })]))
+    .pipe(gulp.dest("source/img"))
 });
 
 gulp.task("server", function () {
